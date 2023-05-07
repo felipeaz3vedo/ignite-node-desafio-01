39aa5415d43281373dd5e727657f9ab58be8234c
Felipe Pessoa <felipedeazevedopessoa>
2023-05-06 22:37:11 -0300

    chore(01): add prettier, eslint and editor config

--- 01-fundamentos-node-js/src/server.js
+++ 01-fundamentos-node-js/src/server.js
@@ -1,6 +1,5 @@
 import http from 'http'
 
-const server = http.createServer((req, res) => {
-})
+const server = http.createServer(() => {})
 
 server.listen(3333, () => console.log('server running on port 3333 🚀'))
